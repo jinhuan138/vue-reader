91634eeb07aa3ec440782a47c1a120787035c1d8
lindacong <2796009827@qq.com>
2023-04-04 23:47:25 +0800

    fix ts

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser'//压缩代码
 import strip from '@rollup/plugin-strip'; //删除log
 import vue from 'rollup-plugin-vue'
+import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
 import RollupClear from 'rollup-plugin-clear'//打包前删除
 import cleanup from 'rollup-plugin-cleanup';//删除注释
@@ -39,6 +40,9 @@ export default {
     babel({
       exclude: '**/node_modules/**',
       babelHelpers: 'bundled'
+    }),
+    typescript({
+      tsconfig: './tsconfig.json'
     }),
     postcss({
       plugins: [autoprefixer(), cssnano()]
