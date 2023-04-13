9bdcd9b78bb4a73bb06524bcc0a8f73e60d44cd6
lindacong <2796009827@qq.com>
2023-04-13 08:34:16 +0800

    修改 todo

--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,7 @@
+---
+layout: docs
+---
+
 # Vue Reader - an easy way to embed a ePub into your webapp
 An vue-reader for vue powered by EpubJS
 
--- docs/router/index.ts
+++ docs/router/index.ts
@@ -2,14 +2,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-    { path: '/', redirect: '/library' },
     { name: "library", path: '/library', component: () => import('comps/Library.vue') },
     { name: "reader", path: '/reader/:id?/:name?', component: () => import('comps/Reader.vue'), props: true },
     { name: "test", path: '/test', component: () => import('comps/Test.vue') },
 ]
 
 const router = createRouter({
-    history: createWebHistory('/vue-reader'),
+    history: createWebHistory('/docs/'),
     routes,
 })
 
