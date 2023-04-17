b9ea23cbc330fc59e518af9865784348bc176cb1
‘lingdacong’ <2796009827@qq.com>
2023-04-17 10:09:36 +0800

    add get book infomation

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,30 +1,15 @@
 <template>
-    <div style="height: 100vh">
-        <VueReader :epubOptions='{
-            allowPopups: true,
-                allowScriptedContent: true,
-                    script: "../../node_modules/medium-zoom/dist/medium-zoom.min.js"
-        }' url="/files/alice.epub" :getRendition="getRendition">
-        </VueReader>
+    <div>
+        <div style="height: 100vh">
+            <VueReader url="/files/alice.epub">
+            </VueReader>
+        </div>
+        <div style="height: 100vh">
+            <VueReader url="/files/啼笑因缘.epub">
+            </VueReader>
+        </div>
     </div>
 </template>
 <script setup>
 import { VueReader } from "vue-reader";
-import { nextTick } from "vue";
-let rendition = null
-const getRendition = val => rendition = val
-
-nextTick(async () => {
-    rendition.hooks.content.register(async (contents, view) => {
-        const { document } = contents
-        const images = [...document.querySelectorAll('img'), ...document.querySelectorAll('image')]
-        contents.window.mediumZoom(images, {
-            // background: '#BADA55',
-            // scrollOffset: 0,
-            // container: 'body',
-            // template: "#reader",
-            // template: '#zoom-template',
-        })
-    })
-})
 </script>
