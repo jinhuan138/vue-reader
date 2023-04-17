36f34a4a0176512a3b6712b143019e3ee04c1f50
lindacong <2796009827@qq.com>
2023-04-17 23:42:06 +0800

    add input file

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,22 +1,34 @@
 <template>
-    <div style='height: 100vh'>
-        <VueReader :epubOptions='{
-            allowPopups: true,
-            allowScriptedContent: true,
-            script: "https://cdn.jsdelivr.net/npm/medium-zoom@1.0.8/dist/medium-zoom.min.js"
-        }'
-            url='/docs/files/alice.epub' :getRendition='getRendition'>
-        </VueReader>
+    <div style='position: relative;height: 10vh'>
+        <div style='height: 100vh' v-if="url">
+            <VueReader :url='url' :title='title' />
+        </div>
+        <input type="file" :multiple="false" accept=".epub" :onchange="onchange" class="input">
     </div>
 </template>
 <script setup>
-let rendition = null
-const getRendition = (val) => {
-    rendition = val
-    rendition.hooks.content.register((contents, view) => {
-        const { document } = contents
-        const images = [...document.querySelectorAll('img'), ...document.querySelectorAll('image')]
-        contents.window.mediumZoom(images)
-    })
-} 
+import { ref } from 'vue'
+import { VueReader } from '@/modules/index'
+
+const url = ref(null), title = ref('')
+const onchange = (e) => {
+    const file = e.target.files[0];
+    const { name } = file
+    title.value = name.replace('.epub', '')
+    if (window.FileReader) {
+        var reader = new FileReader();
+        reader.onloadend = e => url.value = reader.result
+        reader.readAsArrayBuffer(file);
+    }
+}
 </script>
+<style>
+.input {
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+    left: 1rem;
+    z-index: 1;
+    text-align: center;
+}
+</style>
