3ff7f53a77fdc04214ba9f39b1903b2a9615ed45
lindacong <2796009827@qq.com>
2023-05-08 17:42:27 +0800

    update readme.md

--- test/vue2/App.vue
+++ test/vue2/App.vue
@@ -1,28 +1,29 @@
-<script lang="ts">
-import { VueReader } from '../../src/modules/index'
-export default {
-  components: { VueReader },
-  methods: {
-     change(val){
-          console.log('change',val)
-     }
-  },
-  mounted() {
-    const readerRef =this.$refs['VueReader']
-    console.log(readerRef)
-    // this.$on('pdate:location',(epubcifi)=>{
-    //   console.log(epubcifi)
-    // })
-  }
-
-}
-</script>
-
 <template>
   <div style='height: 100vh'>
-    <VueReader ref="VueReader" url='/files/啼笑因缘.epub' @update:location="change" :epubOptions='{
-      flow: "scrolled",
-      manager: "continuous"
-    }'> </VueReader>
+    <VueReader url="/files/啼笑因缘.epub" :location='location' @update:location='locationChange'> </VueReader>
   </div>
 </template>
+
+<script>
+import { VueReader } from 'vue-reader'
+export default {
+  components: { VueReader },
+  data() {
+    return {
+      location: null,
+      firstRenderDone: false
+    }
+  },
+  methods: {
+    locationChange(epubcifi) {
+      if (!this.firstRenderDone) {
+        this.location = localStorage.getItem('book-progress')
+        return this.firstRenderDone = true
+
+      }
+      localStorage.setItem('book-progress', epubcifi)
+      this.location = epubcifi
+    }
+  }
+}
+</script>
