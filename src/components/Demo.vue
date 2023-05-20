82da8aff000dd2157a98a5be1533892b1aad4323
lindacong <2796009827@qq.com>
2023-05-20 21:03:01 +0800

    暂存

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,96 +1,54 @@
 <template>
-    <div style='height: 100vh'>
-        <VueReader url='/files/啼笑因缘.epub' :location='location' :getRendition='getRendition' :tocChanged="tocChanged"
-            @update:location='locationChange'>
-            <template #loadingView>
-                <div class="loading">
-                    加载中...{{ process }}%
-                    <div class="outer">
-                        <div class="inner" :style="{ width: `${process}%` }"></div>
-                    </div>
-                </div>
-            </template>
-        </VueReader>
-    </div>
-    <div class='page'>
-        {{ page }}
+    <div style='position: relative' :style='{ height: url ? "100vh" : "50px" }'>
+        <div style='height: 100vh' v-if="url">
+            <VueReader :url='url' :title='title' :getRendition='getRendition' @update:location='locationChange' />
+        </div>
+        <input type="file" :multiple="false" accept=".epub" @change="onchange" class="input">
     </div>
 </template>
 <script setup>
-import { VueReader } from '@/modules/index'
-import { ref, onMounted, onBeforeMount, } from 'vue'
-
-let rendition = null, toc = []
-const process = ref(0)
-const page = ref('')
-const location = ref(null)
-const firstRenderDone = ref(false)
+import { ref } from 'vue'
+import VueReader from "vue-reader"
+let isAudioOn = false, text = '', rendition
+let isReading = ref(false)
 
 const getRendition = val => {
     rendition = val
-    const book = rendition.book
-    book.on('book:downloadProgress', (percentage) => {
-        // this.updateDownloadProgressBar(percentage)
-        console.warn(percentage)
-    })
+    rendition.hooks.content.register((contents) => {
+        let textContent = contents.document.body.textContent
+        console.log(textContent)
+    });
 }
-const tocChanged = val => toc = val
+const url = ref(null), title = ref('')
 
-const getLabel = (toc, href) => {
-    let label = 'n/a';
-    toc.some(item => {
-        if (item.subitems.length > 0) {
-            const subChapter = getLabel(item.subitems, href);
-            if (subChapter !== 'n/a') {
-                label = subChapter
-                return true
-            }
-        } else if (item.href.includes(href)) {
-            label = item.label
-            return true
-        }
-    })
-    return label;
-}
-const locationChange = (epubcifi) => {
-    location.value = epubcifi
-    if (epubcifi) {
-        const { displayed, href } = rendition.location.start
-        const { cfi } = rendition.location.end
-        if (href !== 'titlepage.xhtml') {
-            const label = getLabel(toc, href)
-            page.value = `${displayed.page}/${displayed.total} ${label}`
-        }
+const onchange = (e) => {
+    const file = e.target.files[0];
+    const { name } = file
+    title.value = name.replace('.epub', '')
+    if (window.FileReader) {
+        var reader = new FileReader();
+        reader.onloadend = e => url.value = reader.result
+        reader.readAsArrayBuffer(file);
     }
+}
+const locationChange = () => {
+    const range = rendition.getRange(rendition.currentLocation().start.cfi);
+    const endRange = rendition.getRange(rendition.currentLocation().end.cfi);
+    range.setEnd(endRange.startContainer, endRange.startOffset);
+    // console.log(range.toString())
+    // text = range.toString().replace(/\s\s/g, '')
+    //     .replace(/\r/g, '')
+    //     .replace(/\n/g, '')
+    //     .replace(/\t/g, '')
+    //     .replace(/\f/g, '')
 }
 </script>
 <style>
-.page {
-    text-align: center;
-    z-index: 1;
-    color: #000;
-}
-
-.loading {
+.input {
     position: absolute;
-    top: 50%;
-    left: 10%;
-    right: 10%;
-    color: #ccc;
-    text-align: center;
-    margin-top: -.5em;
-}
-
-.loading .outer {
-    height: .6rem;
-    width: 80%;
-}
-
-.loading .outer .inner {
-    animation-duration: 3s;
-    transition: width .1s ease;
-    border-radius: 100px;
-    background-color: #eb5732;
-    height: 100%;
+    bottom: 1rem;
+    right: 1rem;
+    left: 1rem;
+    z-index: 1;
 }
 </style>
