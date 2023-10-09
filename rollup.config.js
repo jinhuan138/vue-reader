b4659ba9429e9d4074397f8425fc0e5f9eb8ab0e
lindacong <2796009827@qq.com>
2023-10-09 10:16:38 +0800

    打包cjs、iife格式

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+//https://cn.rollupjs.org/
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
@@ -8,18 +9,30 @@ import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
 import RollupClear from 'rollup-plugin-clear'//打包前删除
 import cleanup from 'rollup-plugin-cleanup';//删除注释
-import externals from "rollup-plugin-node-externals";//声明第三方依赖
+import externals from "rollup-plugin-node-externals";//声明第三方依赖是否打包
 import autoprefixer from 'autoprefixer'//css 加前缀
 import cssnano from "cssnano";//压缩css
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}));
+const name = pkg.name
 
 export default {
   input: 'src/modules/index.ts',
-  output: [{
-    file: './lib/index.min.js',
-    format: 'es',
-    plugins: [terser()],
-    name: 'vue-reader',
-  }],
+  output: [
+    {
+      file: `./lib/${name}.esm.js`,
+      format: 'es',
+    },
+    {
+      file: `./lib/${name}.cjs.js`,
+      format: 'cjs',
+    },
+    {
+      file: `./lib/${name}.global.js`,
+      format: 'iife',
+    },
+  ],
   plugins: [
     RollupClear({
       targets: ['lib'],
@@ -45,7 +58,8 @@ export default {
     postcss({
       plugins: [autoprefixer(), cssnano()]
     }),
+    terser(),
     cleanup()
   ],
-  external: ['vue', 'Epub', 'vue-demi']
+  external: ['vue', 'vue-demi']
 };
