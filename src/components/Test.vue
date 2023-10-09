5d895f222053b1f79150d609050e5e75e81266e0
lindacong <2796009827@qq.com>
2023-10-09 17:51:48 +0800

    添加打包依赖 epubjs

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,20 +1,22 @@
 <template>
-    <div style='height: 100vh'>
-        <VueReader ref="viewer" :epubOptions='{
-            allowScriptedContent: true,
-        }' url='/files/alice.epub' :getRendition="getRendition">
-        </VueReader>
-    </div>
+  <div style="height: 100vh">
+    <vue-reader
+      ref="viewer"
+      :epubOptions="{
+        allowScriptedContent: true,
+      }"
+      url="/files/alice.epub"
+      :getRendition="getRendition"
+    >
+    </vue-reader>
+  </div>
 </template>
 <script setup>
-import { VueReader } from "@/modules/index"
-import {onMounted} from 'vue'
+import { VueReader } from "@/modules/index";
+import { onMounted } from "vue";
 const getRendition = (rendition) => {
-    rendition.themes.default({
-    });
-    rendition.hooks.content.register((contents, view) => {
-    })
-}
-onMounted(()=>{
-})
+  rendition.themes.default({});
+  rendition.hooks.content.register((contents, view) => {});
+};
+onMounted(() => {});
 </script>
